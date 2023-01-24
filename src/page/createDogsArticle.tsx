00a8751f4790137edb83ee2d2408d8389e4b3ac8
shankachu <shanshan.cao@thalesdigital.io>
2023-01-24 15:26:33 +0100

    add basic elements to the new component

--- src/page/createDogsArticle.tsx
+++ src/page/createDogsArticle.tsx
@@ -1,5 +1,35 @@
+import { Form, Stack, Row, Col, FormLabel } from "react-bootstrap"
+import CreatableReactSelect from 'react-select'
+
 function CreateDogArticle() {
-    return (<h1>Create a dog article</h1>)
+    return (
+        <Form>
+            <Stack gap={4}>
+                <Row>
+                    <Col>
+                        <Form.Group className="mb-3" controlId="title">
+                            <Form.Label>Title</Form.Label>
+                            <Form.Control placeholder="golden retriver"></Form.Control>
+                        </Form.Group>
+                    </Col>
+                    <Col>
+                        <Form.Group className="mb-3" controlId="tags">
+                            <Form.Label>tags</Form.Label>
+                            <CreatableReactSelect
+                                isMulti
+                                menuIsOpen={false} 
+                                placeholder="Type a dog type..."
+                            />
+                        </Form.Group>
+                    </Col>
+                </Row>
+                <Form.Group className="mb-3" controlId="body">
+                            <Form.Label>Body</Form.Label>
+                            <Form.Control required as="textarea" rows={15} />
+                </Form.Group>
+            </Stack>
+        </Form>
+    )
 }
 
 export default CreateDogArticle
