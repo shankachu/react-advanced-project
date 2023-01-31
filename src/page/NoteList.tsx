873012b854a41e5beafee89c9bc9b56cdadd70f4
shankachu <shanshan.cao@thalesdigital.io>
2023-01-31 14:53:41 +0100

    in process creating note card, but missing creating note

--- src/page/NoteList.tsx
+++ src/page/NoteList.tsx
@@ -1,16 +1,32 @@
-import { useState } from "react"
+import { useMemo, useState } from "react"
 import { Form, Stack, Row, Col, Button } from "react-bootstrap"
 import { Link, useNavigate } from 'react-router-dom'
 import ReactSelect from "react-select"
 import { Tag } from "../App"
+import { Note } from "../App"
+
+type SimplifiedNote = {
+    tags: Tag[]
+    title: string
+    id: string
+}
 
 type NoteListPropos = {
     availableTags: Tag[]
+    notes: SimplifiedNote[]
 }
 
-export function NoteList ({ availableTags }: NoteListPropos){
+export function NoteList ({ availableTags, notes }: NoteListPropos){
     const [selectedTags, setSelectedTags] = useState<Tag[]>([])
     const [title, setTitle] = useState('')
+    
+    // filter displayed notes by the title
+    const filteredNotes = useMemo(() => {
+        return notes.filter(note => {
+            return (title === '' || note.title.toLowerCase().includes(title.toLocaleLowerCase()))
+            && (selectedTags.length === 0 || selectedTags.every(tag => note.tags.some(noteTag => noteTag.id === tag.id)))
+        })
+    }, [title, selectedTags, notes])
 
     return <>
     <Row className="align-items-center mb-4">
@@ -51,7 +67,18 @@ export function NoteList ({ availableTags }: NoteListPropos){
             </Row>
         </Form>
 
-        // render the note cards
+        <Row xs={1} sm={2} lg={3} className="g-3">
+            {filteredNotes.map(note => (
+                <Col key={note.id}>
+                    <NoteCard id={note.id} title={note.title} tags={note.tags} />
+                </Col>
+            ))}
+        </Row>
     </Row>
     </>
+}
+
+function NoteCard({ id, title, tags }: SimplifiedNote){
+    return <h1>Hi</h1>
+
 }
